144d3dcad7734d96b0db1c58e9826f9a1521007c
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2022-01-30 09:22:01 -0600

    :art: Improved project list page with `<Head>` tags

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
 // Shows the main list of projects.
 export default function ProjectsList() {
     // TODO: Create This
-    return (<html lang="en">
-    <h1>Project List</h1>
-    </html>)
+    return (
+    <html lang="en">
+        <Head>
+            <title>Project List</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="description" content="Contains a list of pages for projects created by TechnoShip123." />
+        </Head>
+        <body>
+            <h1>Project List</h1>
+        </body>
+    </html>
+    )
 }
